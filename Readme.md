6efd916202937fcca7bf46ac9a20d5e982d19ea9
Vanya Pogorelov <ivan.pogorelov@uibk.ac.at>
2026-03-03 15:14:01 +0100

    Readme.md update and examples.

--- Readme.md
+++ Readme.md
@@ -4,8 +4,8 @@ This repository is a general client code for [TK-SVM] (copied into qdata-consume
 a theoretical description of the method.
 See the qdata-consumer directory for more information.
 
-The raw data and the Qiskit circuits for the [paper] can be found in `qubit_implementation` and `qutrit_implementation`
-folders.
+The raw data and the Qiskit circuits for the [paper] can be found in [qubit_implementation] and [qutrit_implementation] folders. 
+See [check_data.py] and [circuits.py] for examples.
 
 
 [//]: # (external links)
@@ -14,9 +14,13 @@ folders.
 
 [paper]: <https://arxiv.org/abs/2408.05017>
 
+[qubit_implementation]: <https://github.com/LodePollet/QTKSVM/tree/main/qubit_implementation>
 
+[qutrit_implementation]: <https://github.com/LodePollet/QTKSVM/tree/main/qutrit_implementation>
 
+[check_data.py]: <https://github.com/LodePollet/QTKSVM/tree/main/qubit_implementation/check_data.py>
 
+[circuits.py]: <https://github.com/LodePollet/QTKSVM/tree/main/qubit_implementation/cluster_model/circuits.py>
 
 
 
--- qubit_implementation/check_data.py
+++ qubit_implementation/check_data.py
@@ -7,19 +7,20 @@ if __name__ == '__main__':
     for model in ['cluster_model', 'aklt_model']:
         file_names = os.listdir(os.path.join(model, 'data'))
 
-        results = []
-        for name in file_names:
-            with open(os.path.join(model, 'data', name), 'r') as file:
-                result = yaml.load(file, Loader=yaml.FullLoader)
-                results.append(result)
-
         print(f'{model} data:')
         print(f"{'File':55} {'Mean':5} | {'Min':5} | {'Max':5}")
 
-        for name, res in zip(file_names, results):
-            fid_mean = np.mean([f['Hellinger fidelity'] for f in res])
-            fid_min = np.min([f['Hellinger fidelity'] for f in res])
-            fid_max = np.max([f['Hellinger fidelity'] for f in res])
+        for name in file_names:
+            with open(os.path.join(model, 'data', name), 'r') as file:
+                # Loading the data from the .yaml file
+                result = yaml.load(file, Loader=yaml.FullLoader)
+
+                # Each file contains measurement outcomes for the trapped-ion experiment and the Qiskit simulation
+                # for each basis for one g value
+
+            fid_mean = np.mean([f['Hellinger fidelity'] for f in result])
+            fid_min = np.min([f['Hellinger fidelity'] for f in result])
+            fid_max = np.max([f['Hellinger fidelity'] for f in result])
             print(f'{name:55} {fid_mean:4.3f} | {fid_min:4.3f} | {fid_max:4.3f}')
 
         print()
